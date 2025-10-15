4fb038bb4fe7fcede67531b2616fafcdb622acf3
armancoder15 <armandevs74@gmail.com>
2025-10-14 23:12:58 -0700

    Solving chapter 4 to 5

---
+++ app.js
@@ -0,0 +1,92 @@
+
+// Chapter 4
+
+
+// Legal name
+
+
+// let username = "Muhammad Arman "
+// let firstname = "Muhammad "
+// let lastname = " Arman "
+// let usernum = " 031012345678";
+
+// console.log("User name =" ,username);
+// console.log("First name =" ,firstname);
+// console.log("Last name =" ,lastname);
+// console.log("User Num =" ,usernum);
+
+
+// Illegal name
+
+
+// let u s = "hello"
+// console.log(u s);
+
+
+// Chapter 5
+
+// Question 1
+
+
+// let num1 = 3
+// let num2 = 5
+// let num3 = 8
+
+// document.writeln("Sum of " + num1 + " and " + num2 + " is " + num3)
+// Question 2
+
+// var add = 12 + 13;
+
+// console.log(add);
+
+
+// var sub = 12 - 13;
+
+// console.log(sub);
+
+// var mul = 12 * 13;
+
+// console.log(mul);
+
+// var divis = 12 / 13;
+
+// console.log(divis);
+
+// Question 3
+
+// var number;
+// document.writeln("Value after variable decelaration is " + number + " ?");
+
+
+// number = 5;
+
+// document.writeln("<br>" + "Initial value " + number);
+
+// number++;
+
+// document.writeln("<br>" + "Value after increament is " + number);
+
+// number = number + 7;
+
+// document.writeln("<br>" + "Value after addition is " + number);
+
+// number--;
+
+// document.writeln("<br>" + "Value after decreament is " + number);
+
+// number = number/12;
+
+// document.writeln("<br>" + "The Reminder is " + number);
+
+
+// Question 4
+
+// let ticket = 600 * 5;
+
+// console.log("Total cost to buy 5 ticket to a movie is " + ticket + "PKR");
+
+// Question 5
+
+
+
+
