ac70493c668e3ca3026ab160b7d710a63763183f
armancoder15 <armandevs74@gmail.com>
2025-10-18 10:55:38 -0700

    solve chapter 4 to 5

--- app.js
+++ app.js
@@ -87,6 +87,101 @@
 
 // Question 5
 
+// var mult = prompt("Enter your table number" ,5)
+
+// console.log(mult , "x" , 1 , "=" , mult * 1);
+// console.log(mult , "x" , 2 , "=" , mult * 2);
+// console.log(mult , "x" , 3 , "=" , mult * 3);
+// console.log(mult , "x" , 4 , "=" , mult * 4);
+// console.log(mult , "x" , 5 , "=" , mult * 5);
+// console.log(mult , "x" , 6 , "=" , mult * 6);
+// console.log(mult , "x" , 7 , "=" , mult * 7);
+// console.log(mult , "x" , 8 , "=" , mult * 8);
+// console.log(mult , "x" , 9 , "=" , mult * 9);
+// console.log(mult , "x" , 10 , "=" , mult * 10);
 
 
 
+
+// Question NO 6
+
+// var feb = (25 * 9 / 5) + 32;
+
+// console.log(" 25C is ",feb,"F");
+
+
+
+// var celius = (70 - 32) * 5 / 9;
+
+// console.log(" 70F is ", celius , "C")
+
+
+
+// Question No 7
+
+
+
+// var priceItem1 = 650;
+// var itemquantity = 3;
+// var priceOfQuantity = 100;
+
+// var quantityofitem2 = 7;
+
+// var shopping = 100;
+
+// console.log("Price 1 is ", priceItem1, "Quantity of items 1 is ", itemquantity, "Price of items 2 is "
+//     , priceOfQuantity, "Quantity of itme 2 is ", quantityofitem2, "Shipping charges is " + shopping, "Total cost of your order is ", priceItem1 * itemquantity * itemquantity);
+
+
+//Question no 8
+
+// document.writeln(`<h1>Mark sheet</h1>`)
+
+// var totalmark = 980;
+// var obtmark = 804;
+
+// document.writeln("Total mark is ", totalmark, "<br>","Obtaind mark is ", obtmark, "<br>","Percentage is ", obtmark/totalmark*100)
+
+// Question no 9 
+
+// document.writeln(`<h1>Pakistan Currency</h1>`)
+
+
+// var usdoller = 10 * 104.80;
+// var saudiriyal = 25 * 28;
+
+// document.writeln("US dolle is " , usdoller , "<br>", "Saudi Riyal is " , saudiriyal,"<br>","The total currency is PKR " ,usdoller+saudiriyal)
+
+
+// Question no 10
+
+
+
+// var artmetic = 10 +5 * 10/2;
+
+// console.log(artmetic);
+// Question no 11
+
+// document.writeln("<h1>Age calculator</h1>")
+
+
+// var currentYear = prompt("Enter current year");
+// var birthYear = prompt("Enter your bith year");
+// var age = currentYear - birthYear;
+
+// document.writeln("Current age ",currentYear,"<br>", "Birth year " , birthYear,"<br>","Your age ",age);
+
+// Question no 13 
+
+
+// var snack = "Fries";
+
+// var currentAge = 17;
+
+// var maxAge = 18;
+
+// var estaimed = 2;
+
+// var time = 365;
+
+// document.writeln("Favourite snack is ", snack ,"<br>","Your current Age ", currentAge,"<br>","Estimated maximum age ",maxAge,"<br>","Amount of snack per day ",estaimed,"<br>","you wil need ", time *estaimed," snack in year")
